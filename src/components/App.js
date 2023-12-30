3ea29b0d6f330f94af7b38d9d81adc67bc7da330
Elise Erickson <elisece717@gmail.com>
2023-12-30 10:30:54 -0500

    adds necessary components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Nav from "./Nav";
+import HogsContainer from "./HogsContainer";
+import AddHogForm from "./AddHogForm";
 
 import hogs from "../porkers_data";
 
@@ -7,6 +9,8 @@ function App() {
 	return (
 		<div className="App">
 			<Nav />
+			<AddHogForm/>
+			<HogsContainer hogs={hogs}/>
 		</div>
 	);
 }
