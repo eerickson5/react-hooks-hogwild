5bde71f3b5c1930aeef720cbc8f6a2c7ea43fd51
Elise Erickson <elisece717@gmail.com>
2023-12-30 18:15:01 -0500

    adds controlled form for pig enrollment

--- src/components/AddHogForm.js
+++ src/components/AddHogForm.js
@@ -2,7 +2,57 @@
 import React from "react";
 
 export default function AddHogForm(){
+
+    const [formData, setFormData] = React.useState({
+        name: "",
+        weight: 0,
+        specialty: "",
+        image: ""
+    })
+
+    function onInput(event){
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        })
+    }
+
     return(
-        <h1>Add Hog Form</h1>
+        <form style={{margin: '15px'}}>
+            
+        <h2>Enroll Your Pig Today!</h2>
+
+            <div class="ui labeled input">
+                <div class="ui label">
+                    Name
+                </div>
+                <input type="text" placeholder="Piggly Wiggly" name="name"
+                value={formData.name} onChange={onInput}/>
+            </div>
+
+            <div class="ui labeled input" style={{margin: "5px"}}>
+                <div class="ui label">
+                    Weight (lbs)
+                </div>
+                <input type="number" placeholder="15" name="weight"
+                value={formData.weight}/>
+            </div>
+
+            <div class="ui labeled input" style={{margin: "5px"}}>
+                <div class="ui label">
+                    Special Talent
+                </div>
+                <input type="text" placeholder="Skydiving" name="specialty"
+                value={formData.specialty}/>
+            </div>
+
+            <div class="ui labeled input" style={{margin: "5px"}}>
+                <div class="ui label">
+                    Selfie URL
+                </div>
+                <input type="text" placeholder="pigphotos.com"  name="image"
+                value={formData.image}/>
+            </div>
+        </form>
     )
 }
